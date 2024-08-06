84d7fe989317c8bfbe1e7e1fc5021f359bb27301
Beena Rathore <Beena Rathore@host.docker.internal>
2024-08-06 12:55:33 +0530

    Done by DP

--- src/com/java/algoNDataStucture/workat/MatrixPaths.java
+++ src/com/java/algoNDataStucture/workat/MatrixPaths.java
@@ -7,4 +7,14 @@ public class MatrixPaths {
 
 	}
 
+	int getNumPaths(int rows, int columns) {
+		int[][] aMatrix = new int[rows + 1][columns + 1];
+		aMatrix[0][1] = 1;
+		for(int i = 1; i <= rows; i++){
+			for(int j = 1; j <=columns; j++){
+				aMatrix[i][j] = aMatrix[i-1][j] + aMatrix[i][j-1];
+			}
+		}
+		return aMatrix[rows][columns];
+	}
 }
