4d397d08dc58b5897914eaf26286d31b02d66096
shaktipravesh <shaktipravesh@yahoo.co.in>
2024-09-20 17:18:29 +0530

    Most significant bit, isPowerOfTwo, countSetBits

---
+++ src/com/java/algoNDataStucture/workat/MathNBits/MostSignificantBit.java
@@ -0,0 +1,31 @@
+package com.java.algoNDataStucture.workat.MathNBits;
+
+public class MostSignificantBit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    int msbSignificance(int n) {
+        int msb = 1;
+        while(n > 1) {
+        	n = n >> 1;
+        	msb = msb << 1;
+        }
+        return msb;
+    }
+    
+    boolean isPowerOfTwo(int n) {
+    	boolean powerOfTwo = false;
+    	int bitCount = 0;
+    	while(n > 0) {
+    		bitCount += n & 1; 
+    		n = n >> 1;
+    	}
+    	if(bitCount == 1) {
+    		powerOfTwo = true; 
+    	}
+    	return powerOfTwo;
+    }
+}
