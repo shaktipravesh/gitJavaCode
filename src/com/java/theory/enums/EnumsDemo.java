fffc4537d02e4fca3c657abb5253428141d10ad4
shaktipravesh <shaktipravesh@yahoo.co.in>
2024-10-04 21:25:14 +0530

    comment removed

--- src/com/java/theory/enums/EnumsDemo.java
+++ src/com/java/theory/enums/EnumsDemo.java
@@ -5,12 +5,6 @@ public class EnumsDemo {
 	public static void main(String[] args) {
 		System.out.println("MONDAY");
 		System.out.println("TUESDAY");
-		System.out.println("TUESDAY");
-		System.out.println("TUESDAY");
-		System.out.println("TUESDAY");
-		System.out.println("TUESDAY");
-		System.out.println("TUESDAY");
-		System.out.println("TUESDAY");
 	}
 
 }
