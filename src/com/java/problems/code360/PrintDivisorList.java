a164aaa9328b22d5b16c2b8686acabaac6bb1d6a
shaktipravesh <shaktipravesh@yahoo.co.in>
2024-12-07 08:53:27 +0530

    print divisor list

---
+++ src/com/java/problems/code360/PrintDivisorList.java
@@ -0,0 +1,30 @@
+package com.java.problems.code360;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class PrintDivisorList {
+    public static void main(String[] args) {
+        System.out.println(printDivisors(10));
+    }
+
+    public static List< Integer > printDivisors(int n) {
+        List<Integer> divisorList = new ArrayList<>();
+        int start = 1;
+        int end;
+        while(start*start <= n) {
+            if(n%start == 0) {
+                end = n/start;
+                divisorList.add(start);
+                if(start != end) {
+                    divisorList.add(end);
+                }
+            }
+            start++;
+        }
+        divisorList.sort(Comparator.comparing(Integer::intValue));
+        return divisorList;
+
+    }
+}
