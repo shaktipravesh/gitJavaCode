805e82b60bc2f29fabfddf8921f3c97dfb390495
shaktipravesh <shaktipravesh@yahoo.co.in>
2025-02-27 08:40:16 +0530

    Daily Temperatures

---
+++ src/com/java/problems/leetcode/medium/DailyTemperatures739.java
@@ -0,0 +1,22 @@
+package com.java.problems.leetcode.medium;
+
+import java.util.Stack;
+
+public class DailyTemperatures739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+        stack.push(0);
+        int count = 0;
+
+        for(int i = 1; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                result[stack.peek()] = i-stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        return result;
+
+    }
+}
