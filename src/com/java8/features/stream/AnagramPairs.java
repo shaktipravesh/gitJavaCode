d7205bc29f763807293db990f0f9a3717d7a3140
shaktipravesh <shaktipravesh@yahoo.co.in>
2025-04-11 11:56:29 +0530

    Anagram Pairs

---
+++ src/com/java8/features/stream/AnagramPairs.java
@@ -0,0 +1,27 @@
+package com.java8.features.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.lang.System.*;
+
+public class AnagramPairs {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("listen", "silent", "enlist", "rat", "tar", "god", "dog");
+
+        Map<String, List<String>> groupedAnagrams = words.stream()
+                .collect(Collectors.groupingBy(word -> {
+                    char[] chars = word.toCharArray();
+                    Arrays.sort(chars);
+                    return new String(chars);
+                }));
+
+        List<List<String>> anagramPairs = groupedAnagrams.values().stream()
+                .filter(group -> group.size() > 1)
+                .toList();
+
+        out.println("Anagram Pairs: " + anagramPairs);
+    }
+}
